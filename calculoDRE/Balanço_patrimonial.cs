245f6316591004fa5d7c65d27a168f3e906e009e
Thiago <thiago.ferreira164@etec.sp.gov.br>
2024-10-10 15:30:31 -0300

    atualizações no balanço patrimonial

---
+++ calculoDRE/Balanço_patrimonial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculoDRE
+{
+    internal class Balanço_patrimonial
+    {
+        public double Ativo { get; set; }
+
+        public double Passivo { get; set; }
+        public double Capital_social { get; set; }
+        public double Caixa {  get; set; }
+        public double Imóvel { get; set; }
+        
+        public double Estoque { get; set; }
+        public double Fornecedores { get; set; }
+
+        public double Emprestimos { get; set; }
+
+        public void calculaAtivo(double caixa, double estoque)
+        {
+             this.Ativo = caixa + estoque; 
+        }
+        public void calculaPassivo(double fornecedores, double lucro) 
+        {
+            this.Passivo = fornecedores + lucro; 
+        }
+    }
+}
+
--- calculoDRE/DRE.cs
+++ calculoDRE/DRE.cs
@@ -8,15 +8,18 @@ namespace calculoDRE
 {
     public class DRE
     {
+        public DRE()
+        {
+        }
+
         public double Receita { get; set; }
         public double Custo { get; set; }
         public double Dre { get; set; }
 
-        public double dre(double receita, double custo)
+        public void dre(double receita, double custo)
         {
             this.Dre = receita - custo;
 
-            return Dre;
         }
     }
 }
