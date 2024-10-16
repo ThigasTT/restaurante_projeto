9f45caff5131c38cb861d2473062aadce5b6ad94
Thiago <thiago.ferreira164@etec.sp.gov.br>
2024-10-16 17:49:18 -0300

    Adicionei os atributos do funcionário

--- RecursosHumanosLibrary/Funcionario.cs
+++ RecursosHumanosLibrary/Funcionario.cs
@@ -8,5 +8,9 @@ namespace RecursosHumanosLibrary
 {
     internal class Funcionario
     {
+        public string Nome { get; set; }
+        public string Idade {  get; set; }
+        public string Cpf { get; set; }
+        public string 
     }
 }
