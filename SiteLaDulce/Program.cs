6799916ff0cae7c2d46bc45ddaa5f30e207a1925
Thiago <thiago.ferreira164@etec.sp.gov.br>
2024-10-16 16:59:57 -0300

    upadtes

--- SiteLaDulce/Program.cs
+++ SiteLaDulce/Program.cs
@@ -10,6 +10,9 @@ namespace SiteLaDulce
     {
         static void Main(string[] args)
         {
+        
+      
+
         }
     }
 }
--- calculofinanceiro/DRE.cs
+++ calculofinanceiro/DRE.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace calculoDRE
 {
     public class DRE
     {
+
         public DRE()
         {
         }
+       // MySqlCommand cmd;
+       // MySqlConnection conexao = new MySqlConnection();
 
         public double Receita { get; set; }
         public double Custo { get; set; }
         public double Dre { get; set; }
 
-        public void dre(double receita, double custo)
+        public void calculoDre(double receita, double custo)
         {
             this.Dre = receita - custo;
 
