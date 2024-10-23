de39fe3648049da6c4a10114ac90b935e332c301
Thiago <thiago.ferreira164@etec.sp.gov.br>
2024-10-23 16:22:56 -0300

    fiz coisas aí

---
+++ RecursosHumanosLibrary2/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursosHumanosLibrary
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Idade {  get; set; }
+        public string Cpf { get; set; }
+        public string 
+    }
+}
---
+++ RecursosHumanosLibrary2/Rh.cs
@@ -0,0 +1,7 @@
+﻿namespace RecursosHumanosLibrary
+{
+    public class Class1
+    {
+
+    }
+}
