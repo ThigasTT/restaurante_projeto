b37c6b73015e6b0a86eea0124f13112f1d1c0e2a
Thiago <thiago.ferreira164@etec.sp.gov.br>
2024-11-04 16:57:44 -0300

    mudei a estrutura dos sistemas

---
+++ Sistemas_La_Dulce/FinanceiroLibrary/Fornecedores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fornecedoresLibrary
+{
+    public class Fornecedores
+    {
+        public int Idforn { get; set; }
+        public string Logradouro { get; set; }
+        public string Cnpj { get; set; }
+
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Tel_C { get; set; }
+        public string Tempo_Contrato { get; set; }
+    }
+}
