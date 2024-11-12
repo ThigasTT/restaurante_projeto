d23dc4587c569fadb7911a122bb829543c782213
Gustavo0Muniz <gustavo.oliveira970@etec.sp.gov.br>
2024-11-12 19:44:24 -0300

    Feito a conexao do site com o banco de dados

---
+++ LaDulce/LaDulce/form_user.php
@@ -0,0 +1,14 @@
+<?php
+include_once "header.php";
+?>
+<form action="form.php" method="post">
+    <label>Nome:</label><input type="text" name="txt_nome" required maxlength="100"><br>
+    <label>Email:</label><input type="email" name="txt_email" required maxlength="100"><br>
+    <label>Data De Reserva:</label><input type="date" name="dt_data" required><br>
+    <label>N° De Pessoas:</label><input type="text" name="txt_end" required maxlength="100"><br>
+    <textarea name="mensagem" id=""></textarea> <input type="submit" value="Cadastrar" required>
+   
+</form>
+<?php
+include_once "footer.php";
+?>
