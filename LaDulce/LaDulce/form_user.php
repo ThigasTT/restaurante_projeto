6843a39a2edf433565afb9272f7b69d6ae39df9d
Rhaul <162928407+RhaulAraujo@users.noreply.github.com>
2024-11-19 16:58:43 -0300

    Update form_user.php

--- LaDulce/LaDulce/form_user.php
+++ LaDulce/LaDulce/form_user.php
@@ -2,11 +2,11 @@
 include_once "header.php";
 ?>
 <form action="form.php" method="post">
-    <label>Nome:</label><input type="text" name="txt_nome" required maxlength="100"><br>
-    <label>Email:</label><input type="email" name="txt_email" required maxlength="100"><br>
-    <label>Data De Reserva:</label><input type="date" name="dt_data" required><br>
-    <label>N° De Pessoas:</label><input type="text" name="txt_end" required maxlength="100"><br>
-    <textarea name="mensagem" id=""></textarea><br><input type="submit" value="Cadastrar" required>
+    <label class="header-p">Nome:</label><input type="text" name="txt_nome" required maxlength="100"><br>
+    <label class="header-p">Email:</label><input type="email" name="txt_email" required maxlength="100"><br>
+    <label class="header-p">Data De Reserva:</label><input type="date" name="dt_data" required><br>
+    <label class="header-p">N° De Pessoas:</label><input type="text" name="txt_end" required maxlength="100"><br>
+    <textarea name="mensagem" id=""></textarea><br><input class="header-p" type="submit" value="Cadastrar" required>
    
 </form>
 <?php
