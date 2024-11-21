58234f6f5d2b2ec1ac4493e7503b81ff1e26f1d8
Gustavo0Muniz <gustavo.oliveira970@etec.sp.gov.br>
2024-11-21 17:41:56 -0300

    Atualizações no site

--- LaDulce/LaDulce/avaliacao_form.php
+++ LaDulce/LaDulce/avaliacao_form.php
@@ -2,11 +2,20 @@
 include_once "index.php";
 ?>
 
+<style>
+  #formm{
+    height: 100vh;
+  width: 100%;
+  
+  align-items: center;
+  justify-content: center; margin-left: 650px;
+  }
+</style>
 
-<form style="margin: 15px; color:#ffffff; background-color : red;  " action="ava_form.php" method="post">
+<form id="formm" style="color:#ffffff;  " action="ava_form.php" method="post">
     <p>DEIXE AQUI SUA AVALIACÃO:</p>
-    <label>Nome:</label><input style="color: #ffffff; " type="text" name="txt_nome" required><br>
-    <label>Email:</label><input style="color: #ffffff;" type="email" name="txt_email" required maxlength="100"><br>
+    <label>Nome:</label><input style="color: #ffffff; " type="text" name="txt_nome" placeholder="Digite seu Nome"><br>
+    <label>Email:</label><input style="color: #ffffff;" type="email" name="txt_email" placeholder="Digite seu Email" required maxlength="100"><br>
     <input type="radio" id="1" name="estrela" value="1">
   <label for="1">⭐</label><br>
   <input type="radio" id="2" name="estrela" value="2">
@@ -18,7 +27,7 @@ include_once "index.php";
   <input type="radio" id="5" name="estrela" value="5">
   <label for="5">⭐⭐⭐⭐⭐</label><br>
   <textarea style="color: #ffffff;" name="avaliacao" id="" placeholder="Comente aqui"></textarea><br>
-  <input style="color: #ffffff;" type="submit" value="Enviar avaliação" required>
+  <input style="color: black;" type="submit" value="Enviar avaliação" required>
 </form>
 <?php
 include_once "footer.php";
